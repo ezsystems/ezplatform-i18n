6de006602ab5ae53bfc1ec3d9184434c584bcf23
mikolaj <mikolaj.adamczyk@ez.no>
2020-04-01 15:38:17 +0200

    Updated command to align it to v.3.0

--- bin/synchronize-translations.sh
+++ bin/synchronize-translations.sh
@@ -9,21 +9,22 @@ echo "ezsystems/ezplatform-admin-ui"
 rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui/*
 cp ./vendor/ezsystems/ezplatform-admin-ui/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui
 
-echo "ezsystems/ezplatform-admin-ui-modules"
-rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui-modules/*
-cp ./vendor/ezsystems/ezplatform-admin-ui-modules/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui-modules
+# the bundle admin-ui-modules moved to admin-ui bundle
 
-echo "ezsystems/ezpublish-kernel"
-rm -f ./vendor/ezsystems/ezplatform-i18n/ezpublish-kernel/*
-cp ./vendor/ezsystems/ezpublish-kernel/eZ/Bundle/EzPublishCoreBundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezpublish-kernel
+# the bundle ezpublish-kernel renamed to ezplatform-kernel
+if [ ! -d "./vendor/ezsystems/ezplatform-kernel" ]; then
+  mkdir "./vendor/ezsystems/ezplatform-i18n/ezplatform-kernel/"
+fi
 
-echo "ezsystems/repository-forms"
-rm -f ./vendor/ezsystems/ezplatform-i18n/repository-forms/*
-cp ./vendor/ezsystems/repository-forms/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/repository-forms
+if [ -d "./vendor/ezsystems/ezplatform-kernel" ]; then
+  echo "ezsystems/ezplatform-kernel"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-kernel/*
+  cp ./vendor/ezsystems/ezplatform-kernel/eZ/Bundle/EzPublishCoreBundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-kernel
+fi
 
-echo "ezsystems/ez-support-tools"
-rm -f ./vendor/ezsystems/ezplatform-i18n/ez-support-tools/*
-cp ./vendor/ezsystems/ez-support-tools/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ez-support-tools
+# the bundle repository-forms is removed
+
+# bundle has no translation anymore
 
 echo "ezsystems/ezplatform-user"
 rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-user/*
@@ -39,11 +40,7 @@ if [ -d "./vendor/ezsystems/date-based-publisher" ]; then
   cp ./vendor/ezsystems/date-based-publisher/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/date-based-publisher
 fi
 
-if [ -d "./vendor/ezsystems/flex-workflow" ]; then
-  echo "ezsystems/flex-workflow"
-  rm -f ./vendor/ezsystems/ezplatform-i18n/flex-workflow/*
-  cp ./vendor/ezsystems/flex-workflow/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/flex-workflow
-fi
+# the bundle flex-workflow is removed
 
 if [ -d "./vendor/ezsystems/ezplatform-page-builder" ]; then
   echo "ezsystems/ezplatform-page-builder"
@@ -75,10 +72,67 @@ if [ -d "./vendor/ezsystems/ezplatform-richtext" ]; then
   cp ./vendor/ezsystems/ezplatform-richtext/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-richtext
 fi
 
+# added
+if [ ! -d "./vendor/ezsystems/ezplatform-i18n/ezplatform-calendar" ]; then
+  mkdir "./vendor/ezsystems/ezplatform-i18n/ezplatform-calendar"
+fi
+
+if [ -d "./vendor/ezsystems/ezplatform-calendar" ]; then
+  echo "ezsystems/ezplatform-calendar"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-calendar/*
+  cp ./vendor/ezsystems/ezplatform-calendar/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-calendar
+fi
+
+# added
+if [ ! -d "./vendor/ezsystems/ezplatform-i18n/ezplatform-content-forms" ]; then
+  mkdir "./vendor/ezsystems/ezplatform-i18n/ezplatform-content-forms"
+fi
+
+if [ -d "./vendor/ezsystems/ezplatform-content-forms" ]; then
+  echo "ezsystems/ezplatform-content-forms"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-content-forms/*
+  cp ./vendor/ezsystems/ezplatform-content-forms/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-content-forms
+fi
+
+# added
+if [ ! -d "./vendor/ezsystems/ezplatform-i18n/ezplatform-site-factory" ]; then
+  mkdir "./vendor/ezsystems/ezplatform-i18n/ezplatform-site-factory"
+fi
+
+if [ -d "./vendor/ezsystems/ezplatform-site-factory" ]; then
+  echo "ezsystems/ezplatform-site-factory"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-site-factory/*
+  cp ./vendor/ezsystems/ezplatform-site-factory/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-site-factory
+fi
+
+# added
+if [ ! -d "./vendor/ezsystems/ezplatform-i18n/ezplatform-version-comparison" ]; then
+  mkdir "./vendor/ezsystems/ezplatform-i18n/ezplatform-version-comparison"
+fi
+
+if [ -d "./vendor/ezsystems/ezplatform-version-comparison" ]; then
+  echo "ezsystems/ezplatform-version-comparison"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-version-comparison/*
+  cp ./vendor/ezsystems/ezplatform-version-comparison/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-version-comparison
+fi
+
+# added
+if [ ! -d "./vendor/ezsystems/ezplatform-i18n/ezplatform-query-fieldtype" ]; then
+  mkdir "./vendor/ezsystems/ezplatform-i18n/ezplatform-query-fieldtype"
+fi
+
+if [ -d "./vendor/ezsystems/ezplatform-query-fieldtype" ]; then
+  echo "ezsystems/ezplatform-query-fieldtype"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-query-fieldtype/*
+  cp ./vendor/ezsystems/ezplatform-query-fieldtype/src/Symfony/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-query-fieldtype
+fi
+
 echo '# Fixing .xlf extensions'
-rename -s '.xliff' '.xlf' vendor/ezsystems/ezplatform-i18n/*/*
+# uncomment this for OSX rename -s '.xliff' '.xlf' vendor/ezsystems/ezplatform-i18n/*/*
+rename 's/\.xliff/.xlf/' vendor/ezsystems/ezplatform-i18n/*/*
 
 echo '# Strip english locale suffix from filename';
-rename -s '.en' '' ./vendor/ezsystems/ezplatform-i18n/*/*
+# uncomment this for OSX  rename -s '.en' '' ./vendor/ezsystems/ezplatform-i18n/*/*
+rename 's/\.en//' vendor/ezsystems/ezplatform-i18n/*/*
 
 echo 'Translation synchronization done !';
